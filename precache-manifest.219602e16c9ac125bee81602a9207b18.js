7874f58c32aaa243f3b065320a5f5c3b45965a2c
miguel.ccader@gmail.com <miguel.ccader@gmail.com>
2019-11-04 23:36:07 -0600

    Updates

---
+++ precache-manifest.219602e16c9ac125bee81602a9207b18.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d90e7c20c3359ec2393a8dd0ce2195c8",
+    "url": "/Laboratorio9/index.html"
+  },
+  {
+    "revision": "10a194901d237df6756a",
+    "url": "/Laboratorio9/static/css/main.22de9252.chunk.css"
+  },
+  {
+    "revision": "c16a8f3f2b90b125cf23",
+    "url": "/Laboratorio9/static/js/2.54f34972.chunk.js"
+  },
+  {
+    "revision": "10a194901d237df6756a",
+    "url": "/Laboratorio9/static/js/main.3d7d3f1e.chunk.js"
+  },
+  {
+    "revision": "5f3dbf94579206403a4b",
+    "url": "/Laboratorio9/static/js/runtime-main.a3753551.js"
+  }
+]);
